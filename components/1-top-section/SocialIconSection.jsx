5b108cfb84e6d9f5e9c1214d73bfb74edc129f83
Jenny <jennyandersen182@gmail.com>
2024-11-05 16:38:07 +0100

    updaed icon paths

--- components/1-top-section/SocialIconSection.jsx
+++ components/1-top-section/SocialIconSection.jsx
@@ -1,20 +1,24 @@
 // SocialIconSection.jsx
-import './TopSection.css' 
+import './TopSection.css'
+import linkedinIcon from '../../src/assets/icons/Btn-linkedin.svg'
+import githubIcon from '../../src/assets/icons/Btn-github.svg'
+import stackoverflowIcon from '../../src/assets/icons/Btn-stackoverflow.svg'
+import instagramIcon from '../../src/assets/icons/Btn-instagram.svg'
 
 export const SocialIconSection = ({ className }) => {
   return (
     <div className={`social-icons ${className || ''}`}>
       <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/icons/Btn-linkedin.svg" alt="LinkedIn" className="social-icon" />
+        <img src={linkedinIcon} alt="LinkedIn" className="social-icon" />
       </a>
       <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/icons/Btn-github.svg" alt="GitHub" className="social-icon" />
+        <img src={githubIcon} alt="GitHub" className="social-icon" />
       </a>
       <a href="https://stackoverflow.com" target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/icons/Btn-stackoverflow.svg" alt="Stack Overflow" className="social-icon" />
+        <img src={stackoverflowIcon} alt="Stack Overflow" className="social-icon" />
       </a>
       <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/icons/Btn-instagram.svg" alt="Instagram" className="social-icon" />
+        <img src={instagramIcon} alt="Instagram" className="social-icon" />
       </a>
     </div>
   );
