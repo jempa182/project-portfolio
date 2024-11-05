97eae51a570eb9dd491979608cd48cbfd064efee
Jenny <jennyandersen182@gmail.com>
2024-11-05 16:28:12 +0100

    updated image name

--- components/1-top-section/SocialIconSection.jsx
+++ components/1-top-section/SocialIconSection.jsx
@@ -5,16 +5,16 @@ export const SocialIconSection = ({ className }) => {
   return (
     <div className={`social-icons ${className || ''}`}>
       <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/icons/Btn - linkedin.svg" alt="LinkedIn" className="social-icon" />
+        <img src="./src/assets/icons/Btn-linkedin.svg" alt="LinkedIn" className="social-icon" />
       </a>
       <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/icons/Btn - github.svg" alt="GitHub" className="social-icon" />
+        <img src="./src/assets/icons/Btn-github.svg" alt="GitHub" className="social-icon" />
       </a>
       <a href="https://stackoverflow.com" target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/icons/Btn - stackoverflow.svg" alt="Stack Overflow" className="social-icon" />
+        <img src="./src/assets/icons/Btn-stackoverflow.svg" alt="Stack Overflow" className="social-icon" />
       </a>
       <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/icons/Btn - instagram.svg" alt="Instagram" className="social-icon" />
+        <img src="./src/assets/icons/Btn-instagram.svg" alt="Instagram" className="social-icon" />
       </a>
     </div>
   );
