fdb1d2f8433b0e9db05518d564a3a7c41f3cbe71
Jenny <jennyandersen182@gmail.com>
2025-01-05 17:39:16 +0100

    added new profile text

--- components/1-top-section/ProfileInfo.jsx
+++ components/1-top-section/ProfileInfo.jsx
@@ -6,7 +6,7 @@ export const ProfileInfo = () => {
     <div className="profile-info">
       <h3>I am Jenny Andersén</h3>
       <h2>Frontend Developer</h2>
-      <p>Sara is an exceptional developer known for her innovative solutions and exceptional coding abilities. She creates user-friendly applications and solves complex issues with ease. Her drive for excellence makes her a valuable asset to any project and a standout in the technology field.</p>
+      <p>Jenny is a Junior frontend developer with growing expertise in JavaScript, React, TypeScript and Node.js. Having worked 8 years as a Digital Designer and Webmaster, she has now transitioned into web development and is ready to bridge the gap between design and development. Her focus is now to create responsive and visually engaging user experiences. Jenny combines technical skills with eyes for detail, and has a constant passion for learning more.</p>
       <SocialIconSection className="social-icons-left" />
     </div>
   );
