b889aa374e0dadcb1cb8dadd685521a5dcdd754e
Jenny <jennyandersen182@gmail.com>
2025-05-21 21:55:09 +0200

    intro update

--- components/1-top-section/ProfileInfo.jsx
+++ components/1-top-section/ProfileInfo.jsx
@@ -6,7 +6,7 @@ export const ProfileInfo = () => {
     <div className="profile-info">
       <h3>I am Jenny Andersén</h3>
       <h2>Frontend Developer</h2>
-      <p>Jenny is a Junior Frontend Developer with growing expertise in JavaScript, React, TypeScript, and Node.js. She is currently finishing a Web Developer Bootcamp, where she has strengthened her abilities in building modern, responsive web applications. Having worked 8 years as a Digital Designer and Webmaster, she has transitioned into web development and is ready to bridge the gap between design and development. Her focus is on creating responsive and visually engaging user experiences. Jenny combines technical skills with an eye for detail and has a constant passion for learning more.</p>
+      <p>Jenny is a Junior Frontend Developer with a background in digital design and a newly completed Web Developer Bootcamp. She brings hands-on experience with JavaScript, React, TypeScript, and Node.js, and is focused on building responsive, accessible, and user-friendly web applications. With 8 years of prior experience as a Digital Designer and Webmaster, Jenny combines technical skills with strong visual intuition, bridging the gap between design and development. Now actively looking for a role, she’s eager to grow as a developer and contribute to creative, collaborative teams.</p>
       <SocialIconSection className="social-icons-left" />
     </div>
   );
